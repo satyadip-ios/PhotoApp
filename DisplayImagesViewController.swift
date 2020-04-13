7a5956769d07d4898cfbd93b8d650fe462dcb91d
Satyadip Singha <satyads1@in.ibm.com>
2020-04-13 19:10:55 +0530

    - Firebase setup - UI Design of Picker view and display view

--- DisplayImagesViewController.swift
+++ DisplayImagesViewController.swift
@@ -12,7 +12,6 @@ class DisplayImagesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
